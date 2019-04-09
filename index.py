b2de5096c10d452f0832852f89e47e997c8423ea
dioptrique <h0910139@gmail.com>
2019-04-09 09:56:04 +0800

    unigram VSM working

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ import getopt
 from indexer import index
 
 def usage():
-    print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
+    print "usage: " + sys.argv[0] + " -i dataset-file -d dictionary-file -p postings-file"
 
-input_directory = output_file_dictionary = output_file_postings = None
+input_file = output_file_dictionary = output_file_postings = None
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], 'i:d:p:')
@@ -17,8 +17,8 @@ except getopt.GetoptError, err:
     sys.exit(2)
     
 for o, a in opts:
-    if o == '-i': # input directory
-        input_directory = a
+    if o == '-i': # input file
+        input_file = a
     elif o == '-d': # dictionary file
         output_file_dictionary = a
     elif o == '-p': # postings file
@@ -26,11 +26,11 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
         
-if input_directory == None or output_file_postings == None or output_file_dictionary == None:
+if input_file == None or output_file_postings == None or output_file_dictionary == None:
     usage()
     sys.exit(2)
 
-index(input_directory, output_file_dictionary, output_file_postings)
+index(input_file, output_file_dictionary, output_file_postings)
 
 ### Test code on smaller number of documents ###
 # print(index("./test_docs","test_dictionary.txt", "test_postings.txt"))
